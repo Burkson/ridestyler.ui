d60eb137a492ce314a0de14c7e9f143eccc7bfb0
Byron Carasco <bcarasco@gmail.com>
2019-10-09 14:20:01 -0700

    Updates gulpfile for gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -7,61 +7,73 @@ var autoprefixer = require('gulp-autoprefixer');
 var pug = require('gulp-pug');
 
 var paths = {
-  source: './src/*.scss',
-  doc: './docs/src/scss/*.scss'
+    source: './src/*.scss',
+    doc: './docs/src/scss/*.scss'
 };
 
-gulp.task('watch', function() {
-  gulp.watch('./**/*.scss', ['build']);
-  gulp.watch('./**/*.scss', ['docs']);
-  gulp.watch('./**/*.pug', ['docs']);
+gulp.task('watch', function () {
+    gulp.watch('./**/*.scss', gulp.series('build'));
+    gulp.watch('./**/*.scss', gulp.series('docs'));
+    gulp.watch('./**/*.pug', gulp.series('docs'));
 });
 
-gulp.task('build', function() {
-  gulp.src(paths.source)
-    .pipe(sass({outputStyle: 'compact', precision: 10})
-      .on('error', sass.logError)
-    )
-    .pipe(autoprefixer())
-    .pipe(csscomb())
-    .pipe(gulp.dest('./dist'))
-    .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./dist'));
+gulp.task('build', function () {
+    return gulp.src(paths.source)
+        .pipe(sass({ outputStyle: 'compact', precision: 10 })
+            .on('error', sass.logError)
+        )
+        .pipe(autoprefixer())
+        .pipe(csscomb())
+        .pipe(gulp.dest('./dist'))
+        .pipe(cleancss())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('docs', function() {
-  gulp.src(paths.doc)
-    .pipe(sass({outputStyle: 'compact', precision: 10})
-      .on('error', sass.logError)
-    )
-    .pipe(autoprefixer())
-    .pipe(csscomb())
-    .pipe(gulp.dest('./docs/dist'))
-    .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./docs/dist'));
-  gulp.src(paths.source)
-    .pipe(sass({outputStyle: 'compact', precision: 10})
-      .on('error', sass.logError)
-    )
-    .pipe(autoprefixer())
-    .pipe(csscomb())
-    .pipe(gulp.dest('./docs/dist'))
-    .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./docs/dist'));
-  gulp.src('docs/src/**/!(_)*.pug')
-    .pipe(pug({
-      pretty: true
-    }))
-    .pipe(gulp.dest('./docs/'));
-});
+var docTasks = {
+    scssDocs() {
+        return gulp.src(paths.doc)
+            .pipe(sass({ outputStyle: 'compact', precision: 10 })
+                .on('error', sass.logError)
+            )
+            .pipe(autoprefixer())
+            .pipe(csscomb())
+            .pipe(gulp.dest('./docs/dist'))
+            .pipe(cleancss())
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(gulp.dest('./docs/dist'))
+    },
+    scssSource() {
+        return gulp.src(paths.source)
+            .pipe(sass({ outputStyle: 'compact', precision: 10 })
+                .on('error', sass.logError)
+            )
+            .pipe(autoprefixer())
+            .pipe(csscomb())
+            .pipe(gulp.dest('./docs/dist'))
+            .pipe(cleancss())
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(gulp.dest('./docs/dist'))
+    },
+    html() {
+        return gulp.src('docs/src/**/!(_)*.pug')
+            .pipe(pug({
+                pretty: true
+            }))
+            .pipe(gulp.dest('./docs/'))
+    }
+};
 
-gulp.task('default', ['build']);
+gulp.task('docs', gulp.parallel(
+    docTasks.html,
+    docTasks.scssDocs,
+    docTasks.scssSource
+));
+
+gulp.task('default', gulp.series('build'))
